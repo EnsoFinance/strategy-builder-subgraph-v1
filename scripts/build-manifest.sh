8b3341f5eb466292cc48272401c81c3eb9dc3e5b
Ben Wolf <benjamineliaswolf@gmail.com>
2022-02-16 16:37:57 +0100

    Update ensonet ip address

--- scripts/build-manifest.sh
+++ scripts/build-manifest.sh
@@ -17,7 +17,7 @@ if [[ "$NETWORK" == "kovan" ]]; then
 fi
 
 if [[ "$NETWORK" == "remote" ]]; then
-    ENSONET_DEPLOYMENTS=$(curl http://3.144.40.241/api/deployments)
+    ENSONET_DEPLOYMENTS=$(curl http://18.223.196.75/api/deployments)
     echo $ENSONET_DEPLOYMENTS | jq '."v1-core"  + {"network":"mainnet","ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' | mustache  - templates/subgraph.template.yaml > subgraph.yaml 
     echo $ENSONET_DEPLOYMENTS | jq '."v1-core"  + {"ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' |  mustache  - templates/addresses.ts > src/addresses.ts
 fi
