400bc67b5c57bf0f65d53a04eff0099e0f92492f
Ben Wolf <benjamineliaswolf@gmail.com>
2022-02-16 20:36:28 +0100

    Fetch latest block from fork when deploying to ensonet

--- scripts/build-manifest.sh
+++ scripts/build-manifest.sh
@@ -7,17 +7,18 @@ DATA=deployments.json
 echo 'Generating manifest from data file: '$DATA 'on' $1;
 
 if [[ "$NETWORK" == "mainnet" ]]; then
-    cat $DATA | node_modules/node-jq/bin/jq '.mainnet  + {"network":"mainnet","ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml
+    cat $DATA | node_modules/node-jq/bin/jq '.mainnet  + {"network":"mainnet","ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf","blockNumber":14220000}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml
     cat $DATA | node_modules/node-jq/bin/jq '.mainnet  + {"ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' |  node_modules/.bin/mustache  - templates/addresses.ts > src/addresses.ts
 fi
 
 if [[ "$NETWORK" == "kovan" ]]; then
-    cat $DATA | node_modules/node-jq/bin/jq '.kovan  + {"network":"kovan","ChainlinkFeedRegistry":"0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0"}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml 
+    cat $DATA | node_modules/node-jq/bin/jq '.kovan  + {"network":"kovan","ChainlinkFeedRegistry":"0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0","blockNumber":14220000}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml 
     cat $DATA | node_modules/node-jq/bin/jq '.kovan  + {"ChainlinkFeedRegistry":"0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0"}' |  node_modules/.bin/mustache  - templates/addresses.ts > src/addresses.ts
 fi
 
 if [[ "$NETWORK" == "remote" ]]; then
+    REMOTE_BLOCK=$(curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_blockNumber","id":1}' http://18.223.196.75 | node_modules/node-jq/bin/jq ".result" | tr -d '"')
     ENSONET_DEPLOYMENTS=$(curl http://18.223.196.75/api/deployments)
-    echo $ENSONET_DEPLOYMENTS | node_modules/node-jq/bin/jq '."v1-core"  + {"network":"mainnet","ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml 
+    echo $ENSONET_DEPLOYMENTS | node_modules/node-jq/bin/jq '."v1-core"  + {"network":"mainnet","ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf","blockNumber":'$(($REMOTE_BLOCK))'}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml 
     echo $ENSONET_DEPLOYMENTS | node_modules/node-jq/bin/jq '."v1-core"  + {"ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' | node_modules/.bin/mustache  - templates/addresses.ts > src/addresses.ts
 fi
