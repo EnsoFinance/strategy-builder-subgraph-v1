c250765c5f4ef746f83c00b709ed9f83ae47ea29
milonite <milonite@hotmail.it>
2022-08-10 08:53:02 +0200

    remove kovan network

--- scripts/build-manifest.sh
+++ scripts/build-manifest.sh
@@ -2,7 +2,7 @@
 
 NETWORK=$1
 
-if [ -z $NETWORK ]; then echo "Network not specified! (mainnet, kovan, local, ensonet, remote)"; exit 0; fi
+if [ -z $NETWORK ]; then echo "Network not specified! (mainnet, local, ensonet, remote)"; exit 0; fi
 
 DATA=deployments.json
 echo 'Generating manifest from data file: '$DATA 'on' $1;
@@ -10,11 +10,6 @@ echo 'Generating manifest from data file: '$DATA 'on' $1;
 if [[ "$NETWORK" == "mainnet" ]]; then
     cat $DATA | node_modules/node-jq/bin/jq '.mainnet  + {"network":"mainnet","ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf","blockNumber":14220000}' | node_modules/.bin/mustache  - templates/subgraph.fork.template.yaml > subgraph.yaml
     cat $DATA | node_modules/node-jq/bin/jq '.mainnet  + {"ChainlinkFeedRegistry":"0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf"}' |  node_modules/.bin/mustache  - templates/addresses.ts > src/addresses.ts
-fi
-
-if [[ "$NETWORK" == "kovan" ]]; then
-    cat $DATA | node_modules/node-jq/bin/jq '.kovan  + {"network":"kovan","ChainlinkFeedRegistry":"0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0","blockNumber":14220000}' | node_modules/.bin/mustache  - templates/subgraph.template.yaml > subgraph.yaml 
-    cat $DATA | node_modules/node-jq/bin/jq '.kovan  + {"ChainlinkFeedRegistry":"0xAa7F6f7f507457a1EE157fE97F6c7DB2BEec5cD0"}' |  node_modules/.bin/mustache  - templates/addresses.ts > src/addresses.ts
 fi
 
 if [[ "$NETWORK" == "local" ]]; then
