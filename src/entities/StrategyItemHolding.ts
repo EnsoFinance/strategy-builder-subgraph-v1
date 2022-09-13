131879b8ef86c5f5ce84606b7d976acac9695a64
milonite <milonite@hotmail.it>
2022-09-13 14:56:38 +0200

    use multicall enso oracle

--- src/entities/StrategyItemHolding.ts
+++ src/entities/StrategyItemHolding.ts
@@ -1,7 +1,7 @@
 import { Address, BigInt, Bytes, log } from '@graphprotocol/graph-ts'
 import { StrategyItemHolding } from '../../generated/schema'
 import { NewStrategyItemsStruct } from '../../generated/StrategyProxyFactory/StrategyProxyFactory'
-import { trackDayData } from './DayData'
+import { trackAllDayData } from './DayData'
 import { ensureToken } from './Tokens'
 import { getTokenBalance } from '../helpers/tokens'
 import { isStrategy, useStrategy } from './Strategy'
@@ -42,7 +42,7 @@ export function trackItemsQuantitiesChange(
   let strategy = useStrategy(strategyAddress.toHexString())
 
   trackStrategyItems(strategy.items, strategyAddress)
-  trackDayData(strategy.id, timestamp)
+  //trackAllDayData(timestamp)
 }
 
 export function createRebalancedItemsHolding(
